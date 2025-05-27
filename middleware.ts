da7102baec2fa9dd4073d5f281679c002a8277ce
Steve Diego TAKOUDJOU <stevediego@honowa.com>
2025-05-27 18:21:57 +0100

    only admin has access to the dashboard

--- middleware.ts
+++ middleware.ts
@@ -25,7 +25,7 @@ export async function middleware(request: NextRequest) {
 	}
 
 	const userRoles = session.user.role || [];
-	const allowedRoles = ["admin", "driver"];
+	const allowedRoles = ["admin"];
 
 	const isAuthorized = userRoles;
 
